a9a7f06bae2547f88eb4c909549abd0fb8ad9e6f
QuocHung-0309 <22110340@student.hcmute.edu.vn>
2025-10-26 03:17:34 +0700

    update

--- app/api/documents/compact/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { listCompact } from "@/lib/static-docs";
-
-export async function GET() {
-  return NextResponse.json({ items: listCompact() });
-}
